ded920809f1e89e4088dc6893c6050d9dd197177
yizel_sanchez <sanchezyizel601@gmail.com>
2025-09-04 20:22:33 -0500

    explicaion clase

---
+++ clase/index.php
@@ -0,0 +1,60 @@
+<?php
+// clase: molde  para crear mas objetos
+
+class Carro{
+    //atributos o propiedades(variables)
+    public $color="rojo";
+    public $marca="ferrari";
+    public $modelo="aventador";
+    public $velocidad= 500;
+    public $caballaje= 300;
+    public $puestos= 2;
+
+    //metodos => acciones que hace el carro(funciones)
+
+    public function getColor(){
+        return $this->color;
+    }
+
+    public function setColor($color){
+        $this->color = $color;
+    }
+
+    public function getVelocidad(){
+        return $this->velocidad;
+    }
+
+    public function setVelocidad($velocidad){
+        $this->velocidad = $velocidad;
+    }
+
+    public function acelerar(){
+        $this->velocidad++;
+    }
+
+    public function frenar(){
+        $this->velocidad--;
+    }
+
+}
+
+// crear un objeto / instanciar una clase
+$carroEspecial = new Carro();
+
+//var_dump($carroEspecial);
+echo "la velocidad de mi carro es: ".$carroEspecial->getVelocidad(). "<br>";
+
+echo "El color de mi caro es: ".$carroEspecial->getColor(). "<br>";
+
+$carroEspecial->setColor("amarillo");
+
+echo $carroEspecial->getColor(). "<br>";
+
+$carroEspecial->acelerar();
+$carroEspecial->acelerar();
+$carroEspecial->acelerar();
+$carroEspecial->acelerar();
+echo "ahora la velocidad de mi carro es: ".$carroEspecial->getVelocidad(). "<br>";
+
+$carroEspecial->frenar();
+echo "ahora la velocidad despues de frenar mi carro: ".$carroEspecial->getVelocidad(). "<br>";
